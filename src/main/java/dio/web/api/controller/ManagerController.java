92912d2c77ea63b5fbca7c2e3363e7b1d93f3261
deafpenguin <victormurilo@gmail.com>
2022-10-27 12:59:51 -0300

    Refatorando autenticação

--- src/main/java/dio/web/api/controller/ManagerController.java
+++ src/main/java/dio/web/api/controller/ManagerController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/managers")
-@PreAuthorize("hasRole('MANAGERS')")
 public class ManagerController {
 
     @Autowired
--- src/main/java/dio/web/api/controller/UsuarioController.java
+++ src/main/java/dio/web/api/controller/UsuarioController.java
@@ -10,7 +10,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/users")
-@PreAuthorize("hasAnyRole('MANAGERS','USERS')")
 public class UsuarioController {
     @Autowired
     private UsuarioRepository repository;
